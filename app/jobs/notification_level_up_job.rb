74f9e0b00a8442c8823ac0b737c6ff140c01ca52
Peter Nguyen <peternguyen1991@gmail.com>
2015-10-01 22:59:48 +0700

    fix redis socket

--- app/jobs/notification_level_up_job.rb
+++ app/jobs/notification_level_up_job.rb
@@ -5,7 +5,7 @@ class NotificationLevelUpJob < ActiveJob::Base
 		redis = Redis.new(:host => Settings.redis_host, :port => Settings.redis_port)
 		socket_id = redis.get("notification:#{email}")
 		if socket_id
-			emitter = SocketIO::Emitter.new(Redis.new(:host => Settings.redis_host, :port => Settings.redis_port))
+			emitter = SocketIO::Emitter.new({redis: Redis.new(:host => Settings.redis_host, :port => Settings.redis_port)})
 			emitter.of("/notification").in(socket_id).emit("levelup", {new_level: new_value})
 		end
 	end
