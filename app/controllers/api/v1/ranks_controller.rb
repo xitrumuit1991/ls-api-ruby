4b3cb903cb364791dcbba8e62c1f8ea4742986ca
Danh Bun An Co <angco89@gmail.com>
2015-10-07 15:51:39 +0700

    ranks month , all , week

--- app/controllers/api/v1/ranks_controller.rb
+++ app/controllers/api/v1/ranks_controller.rb
@@ -15,15 +15,33 @@ class Api::V1::RanksController < Api::V1::ApplicationController
 	end
 
 	def topHeartBroadcaster
-		@top_heart = Broadcaster.order(recived_heart: :desc).limit(10)
+		if params[:range] == "month"
+			@top_heart = Broadcaster.where(updated_at: 1.month.ago..DateTime.now).order(recived_heart: :desc).limit(10)
+		elsif params[:range] == "all"
+			@top_heart = Broadcaster.order(recived_heart: :desc).limit(10)
+		else
+			@top_heart = Broadcaster.where(updated_at: 1.week.ago..DateTime.now).order(recived_heart: :desc).limit(10)
+		end
 	end
 
 	def topLevelGrowBroadcaster
-		@top_broadcaster_level = Broadcaster.order(broadcaster_exp: :desc).limit(10)
+		if params[:range] == "month"
+			@top_broadcaster_level = Broadcaster.where(updated_at: 1.month.ago..DateTime.now).order(broadcaster_exp: :desc).limit(10)
+		elsif params[:range] == "all"
+			@top_broadcaster_level = Broadcaster.order(broadcaster_exp: :desc).limit(10)
+		else
+			@top_broadcaster_level = Broadcaster.where(updated_at: 1.week.ago..DateTime.now).order(broadcaster_exp: :desc).limit(10)
+		end
 	end
 
 	def topLevelGrowUser
-		@top_user_level = User.order(user_exp: :desc).limit(10)
+		if params[:range] == "month"
+			@top_user_level = User.where(updated_at: 1.month.ago..DateTime.now).order(user_exp: :desc).limit(10)
+		elsif params[:range] == "all"
+			@top_user_level = User.order(user_exp: :desc).limit(10)
+		else
+			@top_user_level = User.where(updated_at: 1.week.ago..DateTime.now).order(user_exp: :desc).limit(10)
+		end
 	end
 
 end
