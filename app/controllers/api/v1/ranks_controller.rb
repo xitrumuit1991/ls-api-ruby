2aa560eb1faf83238d850b0ef5de8f97970c45cc
Danh Bun An Co <angco89@gmail.com>
2015-10-12 11:12:47 +0700

    edit ngay update thanh ngay tao

--- app/controllers/api/v1/ranks_controller.rb
+++ app/controllers/api/v1/ranks_controller.rb
@@ -4,11 +4,11 @@ class Api::V1::RanksController < Api::V1::ApplicationController
 
 	def topBroadcasterRevcivedHeart
 		if params[:range] == nil or params[:range] == "week"
-			@top_heart = WeeklyTopBctReceivedHeart.select('*,sum(quantity) as quantity').where(updated_at: DateTime.now.prev_week..DateTime.now.prev_week.sunday).group(:broadcaster_id).order('quantity desc').limit(5)
+			@top_heart = WeeklyTopBctReceivedHeart.select('*,sum(quantity) as quantity').where(created_at: DateTime.now.prev_week.all_week).group(:broadcaster_id).order('quantity desc').limit(5)
 		elsif params[:range] == "all"
 			@top_heart = TopBctReceivedHeart.select('*,sum(quantity) as quantity').group(:broadcaster_id).order('quantity desc').limit(5)
 		elsif params[:range] == "month"
-			@top_heart = MonthlyTopBctReceivedHeart.select('*,sum(quantity) as quantity').where(updated_at: DateTime.now.prev_month.beginning_of_month..DateTime.now.prev_month.end_of_month).group(:broadcaster_id).order('quantity desc').limit(5)
+			@top_heart = MonthlyTopBctReceivedHeart.select('*,sum(quantity) as quantity').where(created_at: DateTime.now.prev_month.all_month).group(:broadcaster_id).order('quantity desc').limit(5)
 		else
 			render plain: 'Range error !', status: 400
 		end
@@ -16,11 +16,11 @@ class Api::V1::RanksController < Api::V1::ApplicationController
 
 	def topBroadcasterRevcivedGift
 		if params[:range] == nil or params[:range] == "week"
-			@top_gift_broadcasters = WeeklyTopBctReceivedGift.select('*,sum(quantity) as quantity, sum(money) as total_money').where(updated_at: DateTime.now.prev_week..DateTime.now.prev_week.sunday).group(:broadcaster_id).order('quantity desc').limit(5)
+			@top_gift_broadcasters = WeeklyTopBctReceivedGift.select('*,sum(quantity) as quantity, sum(money) as total_money').where(created_at: DateTime.now.prev_week.all_week).group(:broadcaster_id).order('quantity desc').limit(5)
 		elsif params[:range] == "all"
 			@top_gift_broadcasters = TopBctReceivedGift.select('*,sum(quantity) as quantity, sum(money) as total_money').group(:broadcaster_id).order('quantity desc').limit(5)
 		elsif params[:range] == "month"
-			@top_gift_broadcasters = MonthlyTopBctReceivedGift.select('*,sum(quantity) as quantity, sum(money) as total_money').where(updated_at: DateTime.now.prev_month.beginning_of_month..DateTime.now.prev_month.end_of_month).group(:broadcaster_id).order('quantity desc').limit(5)
+			@top_gift_broadcasters = MonthlyTopBctReceivedGift.select('*,sum(quantity) as quantity, sum(money) as total_money').where(created_at: DateTime.now.prev_month.all_month).group(:broadcaster_id).order('quantity desc').limit(5)
 		else
 			render plain: 'Range error !', status: 400
 		end
@@ -28,11 +28,11 @@ class Api::V1::RanksController < Api::V1::ApplicationController
 
 	def topUserSendGift
 		if params[:range] == nil or params[:range] == "week"
-			@top_gift_users = WeeklyTopUserSendGift.select('*,sum(quantity) as quantity, sum(money) as total_money').where(updated_at: DateTime.now.prev_week..DateTime.now.prev_week.sunday).group(:broadcaster_id).order('quantity desc').limit(5)
+			@top_gift_users = WeeklyTopUserSendGift.select('*,sum(quantity) as quantity, sum(money) as total_money').where(created_at: DateTime.now.prev_week.all_week).group(:broadcaster_id).order('quantity desc').limit(5)
 		elsif params[:range] == "all"
 			@top_gift_users = TopUserSendGift.select('*,sum(quantity) as quantity, sum(money) as total_money').group(:broadcaster_id).order('quantity desc').limit(5)
 		elsif params[:range] == "month"
-			@top_gift_users = MonthlyTopUserSendGift.select('*,sum(quantity) as quantity, sum(money) as total_money').where(updated_at: DateTime.now.prev_month.beginning_of_month..DateTime.now.prev_month.end_of_month).group(:broadcaster_id).order('quantity desc').limit(5)
+			@top_gift_users = MonthlyTopUserSendGift.select('*,sum(quantity) as quantity, sum(money) as total_money').where(created_at: DateTime.now.prev_month.all_month).group(:broadcaster_id).order('quantity desc').limit(5)
 		else
 			render plain: 'Range error !', status: 400
 		end
@@ -40,11 +40,11 @@ class Api::V1::RanksController < Api::V1::ApplicationController
 
 	def topBroadcasterLevelGrow
 		if params[:range] == nil or params[:range] == "week"
-			@top_broadcaster_level = WeeklyTopBctLevelUp.select('*,sum(times) as level').where(updated_at: DateTime.now.prev_week..DateTime.now.prev_week.sunday).group(:broadcaster_id).order('times desc').limit(5)
+			@top_broadcaster_level = WeeklyTopBctLevelUp.select('*,sum(times) as level').where(created_at: DateTime.now.prev_week.all_week).group(:broadcaster_id).order('times desc').limit(5)
 		elsif params[:range] == "all"
 			@top_broadcaster_level = TopBctLevelUp.select('*,sum(times) as level').group(:broadcaster_id).order('times desc').limit(5)
 		elsif params[:range] == "month"
-			@top_broadcaster_level = MonthlyTopBctLevelUp.select('*,sum(times) as level').where(updated_at: DateTime.now.prev_month.beginning_of_month..DateTime.now.prev_month.end_of_month).group(:broadcaster_id).order('times desc').limit(5)
+			@top_broadcaster_level = MonthlyTopBctLevelUp.select('*,sum(times) as level').where(created_at: DateTime.now.prev_month.all_month).group(:broadcaster_id).order('times desc').limit(5)
 		else
 			render plain: 'Range error !', status: 400
 		end
@@ -52,11 +52,11 @@ class Api::V1::RanksController < Api::V1::ApplicationController
 
 	def topUserLevelGrow
 		if params[:range] == nil or params[:range] == "week"
-			@top_user_level = WeeklyTopUserLevelUp.select('*,sum(times) as level').where(updated_at: DateTime.now.prev_week..DateTime.now.prev_week.sunday).group(:broadcaster_id).order('times desc').limit(5)
+			@top_user_level = WeeklyTopUserLevelUp.select('*,sum(times) as level').where(created_at: Time.now.prev_week.all_week).group(:broadcaster_id).order('times desc').limit(5)
 		elsif params[:range] == "all"
 			@top_user_level = TopUserLevelUp.select('*,sum(times) as level').group(:broadcaster_id).order('times desc').limit(5)
 		elsif params[:range] == "month"
-			@top_user_level = MonthlyTopUserLevelUp.select('*,sum(times) as level').where(updated_at: DateTime.now.prev_month.beginning_of_month..DateTime.now.prev_month.end_of_month).group(:broadcaster_id).order('times desc').limit(5)
+			@top_user_level = MonthlyTopUserLevelUp.select('*,sum(times) as level').where(created_at: DateTime.now.prev_month.beginning_of_month..DateTime.now.prev_month.end_of_month).group(:broadcaster_id).order('times desc').limit(5)
 		else
 			render plain: 'Range error !', status: 400
 		end
