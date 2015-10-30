97f247e84c9b1d8187867c1d1a4900afc7dd975d
Danh Bun An Co <angco89@gmail.com>
2015-10-30 10:16:51 +0700

    vd push code for anco

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,7 @@ Rails.application.routes.draw do
 				post   '/update-profile'   => 'user#updateProfile'
 				post   '/avatar'           => 'user#uploadAvatar'
 				post   '/cover'            => 'user#uploadCover'
+				post   '/room'            => 'room#getPublicRoom'
 			end
 
 			# broadcasters
