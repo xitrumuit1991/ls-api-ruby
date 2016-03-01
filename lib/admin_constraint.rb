119ec3859654d27d722acff475e4123abd68735b
Danh Bun An Co <angco89@gmail.com>
2016-03-01 15:12:52 +0700

    users 10000 xu

---
+++ lib/admin_constraint.rb
@@ -0,0 +1,12 @@
+class AuthConstraint
+  def self.admin?(request)
+    return false unless (cookie = request.cookie_jar['auth'])
+
+    Rails.cache.fetch(cookie['user'], :expires_in => 1.minute) do
+      auth_data = JSON.parse(Base64.decode64(cookie['data']))
+      response = HTTParty.post(Auth.validate_url, :query => auth_data)
+
+      response.code == 200 && JSON.parse(response.body)['roles'].to_a.include?('Admin')
+    end
+  end
+end
