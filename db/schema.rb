53aec19ad63bcd159c33a8937dd852c8ea81cfe8
Thuan Dong <dongtanthuan1992@hotmail.com>
2016-03-29 16:50:56 +0700

    add new fields for table megabank_logs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160323045838) do
+ActiveRecord::Schema.define(version: 20160329094844) do
 
   create_table "action_logs", force: :cascade do |t|
     t.integer  "user_id",        limit: 4
@@ -239,6 +239,8 @@ ActiveRecord::Schema.define(version: 20160323045838) do
     t.integer  "bank_id",       limit: 4
     t.integer  "megabank_id",   limit: 4
     t.integer  "user_id",       limit: 4
+    t.string   "transid",       limit: 255
+    t.string   "mac",           limit: 255
     t.text     "descriptionvn", limit: 65535
     t.text     "descriptionen", limit: 65535
     t.string   "responsecode",  limit: 255
