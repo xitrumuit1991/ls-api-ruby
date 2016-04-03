c42372740af21790ed0a9ba422e634a96477c16e
duc.nguyen <peternguyen1991@gmail.com>
2016-04-04 00:49:13 +0700

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160403170134) do
+ActiveRecord::Schema.define(version: 20160403173352) do
 
   create_table "action_logs", force: :cascade do |t|
     t.integer  "user_id",        limit: 4
