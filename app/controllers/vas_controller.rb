7edafa669fb73b64013143231d35edbbfc584962
duc.nguyen <peternguyen1991@gmail.com>
2016-04-03 22:45:26 +0700

    fix(vas-api) update some mbf_user fieldname

--- app/controllers/vas_controller.rb
+++ app/controllers/vas_controller.rb
@@ -172,7 +172,7 @@ class VasController < ApplicationController
     def subscribe_vip user, vip_package, actived_date
       expiry_date  = actived_date + vip_package.no_day.to_i.day
       user.user_has_vip_packages.update_all(actived: false)
-      user.mobifone_user.update(pkg_code: vip_package.code, pkg_actived: actived_date)
+      user.mobifone_user.update(pkg_code: vip_package.code, active_date: actived_date, expiry_date: expiry_date)
       # TODO: Ghi mobifone_user_charge_log ở đây
       return user.user_has_vip_packages.create(vip_package_id: vip_package.id, actived: true, active_date: actived_date, expiry_date: expiry_date)
     end
@@ -180,7 +180,7 @@ class VasController < ApplicationController
     def create_user phone, password
       if User.exists?(phone: phone)
         @user = User.find_by_phone(phone)
-        @user.create_mobifone_user(id: @user.id, sub_id: phone) if @user.mobifone_user.nil?
+        @user.create_mobifone_user(sub_id: phone) if @user.mobifone_user.nil?
         return true
       else
         activeCode = SecureRandom.hex(3).upcase
@@ -198,7 +198,7 @@ class VasController < ApplicationController
           @user.user_exp       = 0
           @user.actived        = 1
           @user.no_heart       = 0
-          return true if @user.save and @user.create_mobifone_user(id: @user.id, sub_id: phone)
+          return true if @user.save and @user.create_mobifone_user(sub_id: phone)
         end
         return false
       end
