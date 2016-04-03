398b51d8336b141d5200fdeee0a6f752a2a9373a
duc.nguyen <peternguyen1991@gmail.com>
2016-04-03 15:01:32 +0700

    refactor(vas-api) refactor VAS API

--- app/controllers/vas_controller.rb
+++ app/controllers/vas_controller.rb
@@ -3,18 +3,6 @@ class VasController < ApplicationController
 
   soap_service namespace: 'urn:livestar'
 
-  # Mua gói VIP từ VAS
-  # Args:
-  # - id: mã thuê bao
-  # - sub_id: số điện thoại
-  # - pkg_code: mã gói cước (VIP, VIP7, VIP30, VIP2, VIP3, VIP4)
-  # Return:
-  # - error: mã lỗi (nếu 0 là không có lỗi, còn lại xin tham khảo tài liệu mã lỗi)
-  # - message: thông báo trả về trong cả hai trường hợp thành công hoặc lỗi
-  soap_action 'buy_vip_package',
-    args: { id: :integer, sub_id: :string, pkg_code: :string },
-    return: { error: :integer, message: :string}
-
   # Cộng tiền cho thuê bao
   # Args:
   # - sub_id: số điện thoại
@@ -36,57 +24,31 @@ class VasController < ApplicationController
     args: { sub_id: :string },
     return: { error: :integer, message: :string, new_password: :string }
 
-  # Đăng ký tài khoản
+  # Đăng ký tài khoản và kích hoạt VIP
+  # - Nếu chưa có user trên LS thì tạo tài khoản và kích hoạt gói VIP
+  # - Nếu đã có tài khoản những chưa có (hoặc đã hết hạn) gói VIP thì kích hoạt gói VIP
+  # - Nếu đã có tài khoản đã có nói VIP thấp hơn thì nâng cấp gói VIP
   # Args:
   # - sub_id: số điện thoại
   # Return:
-  # - password: mật khẩu (nếu đăng ký thành công)
-  soap_action 'register',
-    args: { sub_id: :string},
-    return: { error: :integer, message: :string, password: :string }
+  # - active_date: ngày kích hoạt gói VIP
+  # - expiry_date: ngày hết hạn gói VIP
+  soap_action 'subscribe',
+    args: { sub_id: :string, pkg_code: :string, password: :string},
+    return: { error: :integer, message: :string, active_date: :string, expiry_date: :string }
 
-  # Gia hạn gói VIP
-  # - Nếu chưa đăng ký gói, thì đăng ký mới
-  # Args:
-  # - sub_id: số điện thoại
-  # - pkg_code: mã gói VIP muốn gia hạn, nếu để trống, tự động gia hạn gói đã active lần cuối cùng
-  # Return:
-  # - pkg_code: gói cước được gia hạn
-  # - actived_date: ngày kích hoạt
-  # - expry_date: ngày hết hạn
-  soap_action 'charge_vip_package',
-    args: { sub_id: :string, pkg_code: :string },
-    return: { error: :integer, message: :string, pkg_code: :string, actived_date: :datetime, expry_date: :datetime }
-
-  # Cập nhật thông tin thuê bao
-  # - Nếu chưa có thì tạo mới (lưu ý phải tạo luôn ở table User của LS)
-  # - Nếu có rồi thì cập nhật
-  # Args: Tham khảo ở bảng mô tả
-  soap_action 'update_subscriber',
-    args: { id: :integer, sub_id: :string, pkg_code: :string, register_chanel: :string, pkg_actived: :datetime, pkg_expried: :datetime, status: :integer },
+  # Gia hạn gói VIP cho thuê bao
+  soap_action 'charge',
+    args: { ub_id: :string },
     return: { error: :integer, message: :string}
 
-  # Cập nhật hàng loạt thuê bao
-  # - Nếu chưa có thì tạo mới (lưu ý phải tạo luôn ở table User của LS)
-  # - Nếu có rồi thì cập nhật
-  # Args: Tham khảo ở bảng mô tả
+  # Gia hạn gói cước cho nhiều thuê bao
   # Return:
   # - successes: list các ID cập nhật hoặc thêm mới thành công
   # - errors: list các ID bị lỗi khi cập nhật
-  soap_action 'update_subscribers',
-    args: [{ id: :integer, sub_id: :string, pkg_code: :string, register_chanel: :string, pkg_actived: :datetime, pkg_expried: :datetime, status: :integer }],
+  soap_action 'mcharge',
+    args: [:string],
     return: { error: :integer, message: :string, errors: [:integer], successes: [:integer]}
-
-
-  def buy_vip_package
-    # TODO
-    render soap: { error: 0, message: 'giao dich thanh cong'}
-  end
-
-  def charge_vip_package
-    # TODO
-    render soap: { error: 0, message: 'gia han thanh cong', pkg_code: 'VIP', actived_date: '2016-04-02 20:44:00', expry_date: '2016-04-03 20:44:00' }
-  end
 
   def add_money
     if params[:sub_id].present? && params[:money].present?
@@ -126,52 +88,89 @@ class VasController < ApplicationController
     render soap: { error: 1, message: 'missing arguments' }
   end
 
-  def register
-    if params[:sub_id].present?
+  def subscribe
+    if params[:sub_id].present? && params[:password].present? && params[:pkg_code].present?
       sub_id = params[:sub_id]
-      if User.exists?(phone: sub_id)
-        render soap: { error: 2, message: "So dien thoai #{sub_id} da duoc dang ky" }
-      else
-        activeCode = SecureRandom.hex(3).upcase
-        user = User.new
-        user.email        = "#{sub_id}@mobifone.com.vn"
-        user.password     = SecureRandom.hex(5)
-        user.active_code  = activeCode
-        user.username     = sub_id
-        user.phone        = sub_id
-        if user.valid?
-          user.name           = sub_id
-          user.birthday       = '2000-01-01'
-          user.user_level_id  = UserLevel.first().id
-          user.money          = 8
-          user.user_exp       = 0
-          user.actived        = 1
-          user.no_heart       = 0
-          if user.save
-            # TODO we need create MobiFoneUser also
-            create_mbf_user user
-            render soap: { error: 0, message: 'dang ky tai khoan thanh cong', password: user.password}
+      password = params[:password]
+      pkg_code = params[:pkg_code]
+      time_now = Time.now
+
+      if create_user sub_id, password
+        vip_package = VipPackage.find_by_code(pkg_code)
+        if vip_package.present?
+          user_has_vip_package = @user.user_has_vip_packages.where(:actived => true)
+          if user_has_vip_package.present?
+            user_vip_package = user_has_vip_package.take.vip_package
+            if user_vip_package.vip.weight < vip_package.vip.weight
+              subscribed = subscribe_vip @user, vip_package, time_now
+              # TODO: Ghi mobifone_user_charge_log ở đây
+              render soap: { error: 0, message: "Da dang ky thanh cong", active_date:  subscribed.active_date, expiry_date: subscribed.expiry_date }
+            elsif user_vip_package.vip.weight == vip_package.vip.weight
+              active_date = user_has_vip_package.take.expiry_date + vip_package.no_day.to_i
+              subscribed = subscribe_vip @user, vip_package, active_date
+              # TODO: Ghi mobifone_user_charge_log ở đây
+              render soap: { error: 0, message: "Da dang ky thanh cong", active_date:  subscribed.active_date, expiry_date: subscribed.expiry_date }
+            else
+              render soap: { error: 3, message: "Tai khoan da dang ky goi VIP cao hon, vui long kiem tra lai" }
+            end
           else
-            render soap: { error: 3, message: 'can\'t create user, contact technical supporter please' }
+            subscribed = subscribe_vip @user, vip_package, time_now
+            # TODO: Ghi mobifone_user_charge_log ở đây
+            render soap: { error: 0, message: "Da dang ky thanh cong", active_date:  subscribed.active_date, expiry_date: subscribed.expiry_date}
           end
         else
-          render json: { error: 4, message: 'invalid user payload' }
+          render soap: { error: 2, message: "Goi cuoc #{pkg_code} khong ton tai, vui long kiem tra lai" }
         end
+      else
+        render soap: { error: 1, message: 'Khong the tao tai khoan, vui long lien he ho tro ky thuat livestar' }
       end
     else
-      render soap: { error: 1, message: 'missing arguments' }
+      render soap: { error: 1, message: 'Vui long nhap day du tham so' }
     end
   end
 
-  def update_subscriber
+  def charge
     # TODO
   end
 
-  def update_subscribers
+  def mcharge
     # TODO
   end
 
   private
+    def subscribe_vip user, vip_package, actived_date
+      expiry_date  = actived_date + vip_package.no_day.to_i.day
+      user.user_has_vip_packages.update_all(actived: false)
+      return user.user_has_vip_packages.create(vip_package_id: vip_package.id, actived: true, active_date: actived_date, expiry_date: expiry_date)
+    end
+
+    def create_user phone, password
+      if User.exists?(phone: phone)
+        @user = User.find_by_phone(phone)
+        return true
+      else
+        activeCode = SecureRandom.hex(3).upcase
+        @user = User.new
+        @user.email        = "#{phone}@mobifone.com.vn"
+        @user.password     = password
+        @user.active_code  = activeCode
+        @user.username     = phone
+        @user.phone        = phone
+        if @user.valid?
+          @user.name           = phone
+          @user.birthday       = '2000-01-01'
+          @user.user_level_id  = UserLevel.first().id
+          @user.money          = 8
+          @user.user_exp       = 0
+          @user.actived        = 1
+          @user.no_heart       = 0
+          # TODO Tạo mobifone_user ở đây
+          return true if @user.save
+        end
+        return false
+      end
+    end
+
     def filter_ip
       puts request.remote_ip
     end
