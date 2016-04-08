5f077cda8dd726514f6f1a9a83241ea84a032697
Van Liem <vanliem2204@gmail.com>
2016-04-08 15:48:07 +0700

    update

--- app/controllers/api/v1/live_controller.rb
+++ app/controllers/api/v1/live_controller.rb
@@ -182,7 +182,7 @@ class Api::V1::LiveController < Api::V1::ApplicationController
 
           user = {id: @user.id, email: @user.email, name: @user.name, username: @user.username}
           emitter = SocketIO::Emitter.new({redis: Redis.new(:host => Settings.redis_host, :port => Settings.redis_port)})
-          emitter.of("/room").in(@room.id).emit("gifts recived", {gift: {id: gift_id, name: dbGift.name, image: dbGift.image.square}, quantity:quantity, total: total, sender: user})
+          emitter.of("/room").in(@room.id).emit("gifts recived", {gift: {id: gift_id, name: dbGift.name, image: dbGift.image_url}, quantity:quantity, total: total, sender: user})
 
           # insert log
           @user.gift_logs.create(room_id: @room.id, gift_id: gift_id, quantity: quantity, cost: total)
