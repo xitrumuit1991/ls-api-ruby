22b850f1c3e786533d7171a9c17b6b2ec9003456
Thuan Dong <dongtanthuan1992@hotmail.com>
2016-05-12 15:25:08 +0700

    acp module report

--- app/controllers/acp/reports_controller.rb
+++ app/controllers/acp/reports_controller.rb
@@ -4,48 +4,72 @@ class Acp::ReportsController < Acp::ApplicationController
   end
 
   def users
-  	if defined? params[:start_date]
-  		puts 'aaaaaaaaaa'
-  	else
-  		puts 'bbbbbbbbbb'
-  	end
-
-  	if !params[:start_date].present? && !params[:end_date].present?
-  		puts '1111111111'
-  		where = "created_at >= '#{Time.zone.now.beginning_of_day}'"
-  	elsif params[:start_date].present? && !params[:start_date].empty? && params[:end_date].present? && !params[:end_date].empty?
-  		puts '2222222222'
-  		where = "created_at between '#{params[:start_date]}'' and '#{params[:end_date]}'"
-  	else
-  		puts '3333333333'
-  		redirect_to({ action: 'users' }, alert: 'Vui lòng chọn ngày bắt đầu và ngày kết thúc !')
-  	end
+  	where = Hash.new
+  	where['created_at'] = Time.zone.now.beginning_of_day..Time.zone.now.end_of_day
 
   	if params[:start_date].present? && params[:end_date].present?
-  		where = "created_at between '#{params[:start_date]}'' and '#{params[:end_date]}'"
-  	elsif params[:start_date].present? && params[:end_date].present?
-  			
-  	else
-  		where = "created_at >= '#{Time.zone.now.beginning_of_day}'"
+  		where['created_at'] = Time.parse(params[:start_date])..Time.parse(params[:end_date])
+  	elsif params[:start_date].present? && !params[:end_date].present? or !params[:start_date].present? && params[:end_date].present?
+  		redirect_to({ action: 'users' }, alert: 'Vui lòng chọn ngày bắt đầu và ngày kết thúc !') and return
   	end
-  			
 
-  	if params[:format].present? && params[:format] == 'xlxs'
-  		@users = User.all.where(where).order('id desc')
-  	else
-  		@users = User.all.where(where).order('id desc').page params[:page]
-  	end
-  	puts '============'
-  	puts params
-  	puts '============'
+  	@total = User.where(where).count
+  	@users = params[:format].present? ? User.where(where).order('id desc') : User.where(where).order('id desc').page(params[:page])
     respond_to do |format|
       format.html
       format.xlsx
     end
-    # render xlsx: "reports/users"
-    # render "acp/reports/users.xlsx.axlsx"
-    # render :xlsx => "users", template: "acp/reports/users"
-    # render :xlsx => "users", :filename => "all_users.xlsx"
+  end
+
+  def user_online
+  	where = Hash.new
+  	where['last_login'] = Time.zone.now.beginning_of_day..Time.zone.now.end_of_day
+
+  	if params[:start_date].present? && params[:end_date].present?
+  		where['last_login'] = Time.parse(params[:start_date])..Time.parse(params[:end_date])
+  	elsif params[:start_date].present? && !params[:end_date].present? or !params[:start_date].present? && params[:end_date].present?
+  		redirect_to({ action: 'users' }, alert: 'Vui lòng chọn ngày bắt đầu và ngày kết thúc !') and return
+  	end
+
+  	@users = params[:format].present? ? User.where(where).order('id desc') : User.where(where).order('id desc').page(params[:page])
+    respond_to do |format|
+      format.html
+      format.xlsx
+    end
+  end
+
+  def user_send_gifts
+  	where = Hash.new
+  	where['created_at'] = Time.zone.now.beginning_of_day..Time.zone.now.end_of_day
+
+  	if params[:start_date].present? && params[:end_date].present?
+  		where['created_at'] = Time.parse(params[:start_date])..Time.parse(params[:end_date])
+  	elsif params[:start_date].present? && !params[:end_date].present? or !params[:start_date].present? && params[:end_date].present?
+  		redirect_to({ action: 'users' }, alert: 'Vui lòng chọn ngày bắt đầu và ngày kết thúc !') and return
+  	end
+
+  	@data = params[:format].present? ? GiftLog.select('user_id, sum(quantity) as quantity').where(where).group(:user_id).order('quantity DESC') : GiftLog.select('user_id, sum(quantity) as quantity').where(where).group(:user_id).order('quantity DESC').page(params[:page])
+    respond_to do |format|
+      format.html
+      format.xlsx
+    end
+  end
+
+  def user_send_hearts
+  	where = Hash.new
+  	where['created_at'] = Time.zone.now.beginning_of_day..Time.zone.now.end_of_day
+
+  	if params[:start_date].present? && params[:end_date].present?
+  		where['created_at'] = Time.parse(params[:start_date])..Time.parse(params[:end_date])
+  	elsif params[:start_date].present? && !params[:end_date].present? or !params[:start_date].present? && params[:end_date].present?
+  		redirect_to({ action: 'users' }, alert: 'Vui lòng chọn ngày bắt đầu và ngày kết thúc !') and return
+  	end
+
+  	@data = params[:format].present? ? HeartLog.select('user_id, sum(quantity) as quantity').where(where).group(:user_id).order('quantity DESC') : HeartLog.select('user_id, sum(quantity) as quantity').where(where).group(:user_id).order('quantity DESC').page(params[:page])
+    respond_to do |format|
+      format.html
+      format.xlsx
+    end
   end
 
   def idols
@@ -64,6 +88,80 @@ class Acp::ReportsController < Acp::ApplicationController
 
   	@idols = Broadcaster.where(deleted: 0)
 		@rooms = params[:format].present? ? Room.joins(:user_logs).select("rooms.broadcaster_id, sum(user_logs.money) as total").where(where).group(:broadcaster_id) : Room.joins(:user_logs).select("rooms.broadcaster_id, sum(user_logs.money) as total").where(where).group(:broadcaster_id).page(params[:page])
+    respond_to do |format|
+      format.html
+      format.xlsx
+    end
+  end
+
+  def idol_receive_hearts
+  	where = Hash.new
+  	where['created_at'] = Time.zone.now.beginning_of_day..Time.zone.now.end_of_day
+
+  	if params[:start_date].present? && params[:end_date].present?
+  		where['created_at'] = Time.parse(params[:start_date])..Time.parse(params[:end_date])
+  	elsif params[:start_date].present? && !params[:end_date].present? or !params[:start_date].present? && params[:end_date].present?
+  		redirect_to({ action: 'users' }, alert: 'Vui lòng chọn ngày bắt đầu và ngày kết thúc !') and return
+  	end
+
+  	@data = params[:format].present? ? HeartLog.select('room_id, sum(quantity) as quantity').where(where).group(:room_id).order('quantity DESC') : HeartLog.select('room_id, sum(quantity) as quantity').where(where).group(:room_id).order('quantity DESC').page(params[:page])
+    respond_to do |format|
+      format.html
+      format.xlsx
+    end
+  end
+
+  def sms
+  	where = Hash.new
+  	where[:phone] = params[:phone] if params[:phone].present?
+  	where[:created_at] = Time.zone.now.beginning_of_day..Time.zone.now.end_of_day
+
+  	if params[:start_date].present? && params[:end_date].present?
+  		where[:created_at] = Time.parse(params[:start_date])..Time.parse(params[:end_date])
+  	elsif params[:start_date].present? && !params[:end_date].present? or !params[:start_date].present? && params[:end_date].present?
+  		redirect_to({ action: 'users' }, alert: 'Vui lòng chọn ngày bắt đầu và ngày kết thúc !') and return
+  	end
+
+  	@total = SmsLog.select('sum(amount) as total').where(where).take
+  	@data = params[:format].present? ? SmsLog.where(where) : SmsLog.where(where).page(params[:page])
+    respond_to do |format|
+      format.html
+      format.xlsx
+    end
+  end
+
+  def card
+  	where = Hash.new
+  	where_name = "users.name LIKE '%#{params[:name]}%'"
+  	where[:created_at] = Time.zone.now.beginning_of_day..Time.zone.now.end_of_day
+
+  	if params[:start_date].present? && params[:end_date].present?
+  		where[:created_at] = Time.parse(params[:start_date])..Time.parse(params[:end_date])
+  	elsif params[:start_date].present? && !params[:end_date].present? or !params[:start_date].present? && params[:end_date].present?
+  		redirect_to({ action: 'users' }, alert: 'Vui lòng chọn ngày bắt đầu và ngày kết thúc !') and return
+  	end
+
+  	@total = CartLog.joins(:user).select('sum(price) as total').where(where).where(where_name).take
+  	@data = params[:format].present? ? CartLog.joins(:user).where(where).where(where_name) : CartLog.joins(:user).where(where).where(where_name).page(params[:page])
+    respond_to do |format|
+      format.html
+      format.xlsx
+    end
+  end
+
+  def bank
+  	where = Hash.new
+  	where_name = "users.name LIKE '%#{params[:name]}%'"
+  	where[:created_at] = Time.zone.now.beginning_of_day..Time.zone.now.end_of_day
+
+  	if params[:start_date].present? && params[:end_date].present?
+  		where[:created_at] = Time.parse(params[:start_date])..Time.parse(params[:end_date])
+  	elsif params[:start_date].present? && !params[:end_date].present? or !params[:start_date].present? && params[:end_date].present?
+  		redirect_to({ action: 'users' }, alert: 'Vui lòng chọn ngày bắt đầu và ngày kết thúc !') and return
+  	end
+
+  	@total = MegabankLog.joins(:user, :megabank).select('sum(megabanks.price) as total').where(where).where("#{where_name} and responsecode = '00'").take
+  	@data = params[:format].present? ? MegabankLog.joins(:user).where(where).where(where_name) : MegabankLog.joins(:user).where(where).where(where_name).page(params[:page])
     respond_to do |format|
       format.html
       format.xlsx
