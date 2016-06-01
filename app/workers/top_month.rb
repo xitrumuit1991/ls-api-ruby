9e818037347c1da6ae543a622ef17af88cc6b856
Danh Bun An Co <angco89@gmail.com>
2016-06-01 11:17:42 +0700

    edit top

--- app/workers/top_month.rb
+++ app/workers/top_month.rb
@@ -3,18 +3,19 @@ class TopMonth
 	include Sidetiq::Schedulable
 	recurrence { weekly.day(:monday).hour_of_day(2).minute_of_hour(0) }
 	def perform()
-		MonthlyTopBctReceivedHeart.destroy_all
-		MonthlyTopBctReceivedHeart.connection.execute("ALTER TABLE monthly_top_bct_received_hearts AUTO_INCREMENT = 1")
-		hearts = HeartLog.select('room_id, sum(quantity) as quantity').where(created_at: DateTime.now.beginning_of_month..DateTime.now).group(:room_id).order('quantity DESC').limit(5)
+		WeeklyTopBctReceivedHeart.destroy_all
+		WeeklyTopBctReceivedHeart.connection.execute("ALTER TABLE top_bct_received_hearts AUTO_INCREMENT = 1")
+		hearts = HeartLog.select('room_id, sum(quantity) as quantity').where(created_at: 1.week.ago.beginning_of_week..1.week.ago.end_of_week).group(:room_id).order('quantity DESC').limit(5)
 		hearts.each do |heart|
-			MonthlyTopBctReceivedHeart.create(:broadcaster_id => heart.room.broadcaster.id, :quantity => heart.quantity)
+			WeeklyTopBctReceivedHeart.create(:broadcaster_id => heart.room.broadcaster.id, :quantity => heart.quantity)
 		end
-		
-		MonthlyTopUserSendGift.destroy_all
-		MonthlyTopUserSendGift.connection.execute("ALTER TABLE monthly_top_user_send_gifts AUTO_INCREMENT = 1")
-		monthly_user_logs = UserLog.select('user_id, sum(money) as money').where(created_at: DateTime.now.beginning_of_month..DateTime.now).group(:user_id).order('money DESC').limit(5)
-		monthly_user_logs.each do |monthly_user_log|
-			MonthlyTopUserSendGift.create(:user_id => monthly_user_log.user_id, :money => monthly_user_log.money)
+
+		WeeklyTopUserSendGift.destroy_all
+		WeeklyTopUserSendGift.connection.execute("ALTER TABLE weekly_top_user_send_gifts AUTO_INCREMENT = 1")
+		weekly_user_logs = UserLog.select('user_id, sum(money) as money').where(created_at: 1.week.ago.beginning_of_week..1.week.ago.end_of_week).group(:user_id).order('money DESC').limit(5)
+		weekly_user_logs.each do |weekly_user_log|
+			WeeklyTopUserSendGift.create(:user_id => weekly_user_log.user_id, :money => weekly_user_log.money)
 		end
+		return head 200
 	end
 end
