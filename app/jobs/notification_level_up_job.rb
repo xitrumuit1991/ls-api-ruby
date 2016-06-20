f065636b9f338f8dd04d6b5d1169e19880a0d48f
Duc Nguyen <duc.nguyen@puresolutions.com.vn>
2016-06-21 00:50:31 +0700

    optimize, caching

--- app/jobs/notification_level_up_job.rb
+++ app/jobs/notification_level_up_job.rb
@@ -2,11 +2,9 @@ class NotificationLevelUpJob < ActiveJob::Base
   queue_as :default
 
   def perform(email, new_value)
-		redis = Redis.new(:host => Settings.redis_host, :port => Settings.redis_port)
-		socket_id = redis.get("notification:#{email}")
+		socket_id = $redis.get("notification:#{email}")
 		if socket_id
-			emitter = SocketIO::Emitter.new({redis: Redis.new(:host => Settings.redis_host, :port => Settings.redis_port)})
-			emitter.of("/notification").in(socket_id).emit("levelup", {new_level: new_value})
+			$emitter.of("/notification").in(socket_id).emit("levelup", {new_level: new_value})
 		end
 	end
 end
