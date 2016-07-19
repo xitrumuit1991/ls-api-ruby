191d79849ef7f0ecc6630c36310d75c2b63247a2
Danh Bun An Co <angco89@gmail.com>
2016-07-19 17:38:05 +0700

    cap nhat doc

--- app/controllers/api/v1/device_controller.rb
+++ app/controllers/api/v1/device_controller.rb
@@ -5,11 +5,8 @@ class Api::V1::DeviceController < Api::V1::ApplicationController
   def register
     device = DeviceToken.find_by_device_token(params[:device_token])
     if device.nil?
-      if DeviceToken.create(user_id: @user.id, device_token: params[:device_token], device_type: params[:type])
-        return head 201
-      else
-        render json: {error: 'error_auth'}, status: 401
-      end
+      DeviceToken.create(user_id: @user.id, device_token: params[:device_token], device_type: params[:type])
+      return head 201
     else
       if device.user_id != @user.id
         device.user_id = @user.id
