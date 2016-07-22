bcd2bd318fef4e191cbd63d0b373bd4fb08870a7
Danh Bun An Co <angco89@gmail.com>
2016-07-22 18:23:09 +0700

    edit devive register

--- app/jobs/device_notification_job.rb
+++ app/jobs/device_notification_job.rb
@@ -6,6 +6,7 @@ class DeviceNotificationJob < ActiveJob::Base
     list_android_tokens = []
     title = 'Livestar'
     description = 'Idol '+user.broadcaster.fullname+ ' xinh đẹp đang online, vào chém gió cùng Idol nào các bạn!'
+    type = 'onair'
     room_id = user.broadcaster.public_room.id
     user.broadcaster.user_follow_bcts.each do |user_follow_bct|
       user_follow_bct.user.device_tokens.each do |device|
@@ -20,7 +21,7 @@ class DeviceNotificationJob < ActiveJob::Base
     	uri = URI.parse(Settings.url_notification+"/ios")
 	    http = Net::HTTP.new(uri.host,uri.port)
 		request = Net::HTTP::Post.new(uri.request_uri, 'Content-Type' => 'application/json')
-	    request.body = {'title' => title, 'description' => description, 'room_id' => room_id, 'tokens' => list_ios_tokens}.to_json
+	    request.body = {'title' => title, 'description' => description, 'room_id' => room_id, 'type' => type, 'tokens' => list_ios_tokens}.to_json
 		request["Content-Type"] = "application/json"
 		http.request(request)
     end
@@ -28,7 +29,7 @@ class DeviceNotificationJob < ActiveJob::Base
       	uri = URI.parse(Settings.url_notification+"/android")
 	    http = Net::HTTP.new(uri.host,uri.port)
 		request = Net::HTTP::Post.new(uri.request_uri, 'Content-Type' => 'application/json')
-	    request.body = {'title' => title, 'description' => description, 'room_id' => room_id, 'tokens' => list_android_tokens}.to_json
+	    request.body = {'title' => title, 'description' => description, 'room_id' => room_id, 'type' => type, 'tokens' => list_android_tokens}.to_json
 		request["Content-Type"] = "application/json"
 		http.request(request)
     end
