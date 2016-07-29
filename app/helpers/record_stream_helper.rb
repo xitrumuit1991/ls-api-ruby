1f9d7529a606d637625f121f8dda7ed205536233
Danh Bun An Co <angco89@gmail.com>
2016-07-29 15:42:06 +0700

    ghi log

--- app/helpers/record_stream_helper.rb
+++ app/helpers/record_stream_helper.rb
@@ -1,7 +1,7 @@
 module RecordStreamHelper
   def start_stream room
-    $redis.set("stream_room_id:#{room.id}", {year: Time.now.year.to_s, month: Time.now.month.to_s, day: Time.now.day.to_s, hour: Time.now.hour.to_s})
-    linkRecode = "#{Settings.url_stream}#{room.id.to_s}&outputFile=#{room.id.to_s}_#{Time.now.year.to_s}_#{Time.now.month.to_s}_#{Time.now.day.to_s}_#{Time.now.hour.to_s}.mp4&option=overwrite&action=startRecording"
+    $redis.set("stream_room_id:#{room.id}", {year: Time.now.year.to_s, month: Time.now.month.to_s, day: Time.now.day.to_s, hour: Time.now.hour.to_s+'_'+Time.now.min.to_s})
+    linkRecode = "#{Settings.url_stream}#{room.id.to_s}&outputFile=#{room.id.to_s}_#{Time.now.year.to_s}_#{Time.now.month.to_s}_#{Time.now.day.to_s}_#{Time.now.hour.to_s+'_'+Time.now.min.to_s}.mp4&option=overwrite&action=startRecording"
     stream_logger = Logger.new("#{Rails.root}/log/StartStream.log")
     stream_logger.info("ANGCO DEBUG StartLinkRecode: #{linkRecode} \n")
     stream_logger.info("ANGCO DEBUG room: #{room} \n")
@@ -14,8 +14,8 @@ module RecordStreamHelper
     stream_logger = Logger.new("#{Rails.root}/log/StopStream.log")
     stream_logger.info("ANGCO DEBUG room: #{room} \n")
     stream_logger.info("ANGCO DEBUG room: #{redis_stream} \n")
-    linkRecode = "#{Settings.url_stream}#{room.id}&outputFile=#{room.id}_#{redis_stream.year}_#{redis_stream.month}_#{redis_stream.day}_#{redis_stream.hour}.mp4&option=overwrite&action=startRecording"
-    linkVideo = "#{Settings.url_video_stream}#{room.id}_#{redis_stream.year}_#{redis_stream.month}_#{redis_stream.day}_#{redis_stream.hour}.mp4/playlist.m3u8"
+    linkRecode = "#{Settings.url_stream}#{room.id.to_s}&outputFile=#{room.id.to_s}_#{redis_stream[:year]}_#{redis_stream[:month]}_#{redis_stream[:day]}_#{redis_stream[:hour]}.mp4&option=overwrite&action=startRecording"
+    linkVideo = "#{Settings.url_video_stream}#{room.id.to_s}_#{redis_stream[:year]}_#{redis_stream[:month]}_#{redis_stream[:day]}_#{redis_stream[:hour]}.mp4/playlist.m3u8"
     stream_logger.info("ANGCO DEBUG StopLinkRecode: #{linkRecode} \n")
     stream_logger.info("ANGCO DEBUG room: #{linkVideo} \n")
     recode linkRecode 
