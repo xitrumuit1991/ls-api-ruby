009715d04d87eeaad6dd547df74a54304306bff2
vdnguyen <vdnguyen3012@gmail.com>
2016-11-29 11:38:43 +0700

    mbuy update function receiveChargingResult

--- app/controllers/mbuy_controller.rb
+++ app/controllers/mbuy_controller.rb
@@ -11,17 +11,16 @@ class MbuyController < ApplicationController
     if params[:totalAmount].present? && params[:isdn].present? && params[:result].present?
       isdn = params[:isdn]
       isdn = '84' + isdn[1..isdn.length]
-      money =  params[:totalAmount].to_i * 0.8
+      money =  params[:totalAmount].to_i * 0.008
       result_parts = params[:result].split('|')
 
       mbuy_request = MbuyRequest.create(command: params[:command], cp_code: params[:cpCode], content_code: params[:ContentCode], total_amount: params[:totalAmount], account: params[:account], isdn: params[:isdn], result: params[:result])
 
       if result_parts[1].present? && result_parts[1] == 'MPAY-0000'
-        user = User.find_by_phone(isdn)
-        if user.present?
-          transaction = MbuyTransaction.create(trans_id: result_parts[0], isdn: isdn, total_amount: params[:totalAmount], checksum: money, user_id: user.id, status: 0)
+        transaction = MbuyTransaction.find_by_trans_id(trans_id: result_parts[0])
+        if transaction.present?
           new_money = user.money + money.to_i;
-          if user.update(money: new_money)
+          if transaction.user.update(money: new_money)
             mbuy_request.update(total_final: money)
             transaction.update(status: 1)
             render soap: { receiveChargingResultResult: 'Cong tien thanh cong'} and return
@@ -29,7 +28,7 @@ class MbuyController < ApplicationController
             render soap: { receiveChargingResultResult: 'Khong the cong tien, vui long lien he ky thuat vien' } and return
           end
         else
-          render soap: { receiveChargingResultResult: "So dien thoai #{isdn} khong ton tai" } and return
+          render soap: { receiveChargingResultResult: 'Transaction khong ton tai'} and return
         end
       else
         user = User.find_by_active_code(params[:account])
@@ -44,7 +43,7 @@ class MbuyController < ApplicationController
             render soap: { receiveChargingResultResult: 'Khong the cong tien, vui long lien he ky thuat vien' } and return
           end
         else
-          render soap: { receiveChargingResultResult: "Active code #{params[:account]} khong ton tai" } and return
+          render soap: { receiveChargingResultResult: "Account khong ton tai" } and return
         end
       end
     else
