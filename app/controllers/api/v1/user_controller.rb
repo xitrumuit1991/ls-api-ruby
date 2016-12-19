a9f2210f5d24cacdb606ad159fc7a388d9c31e24
Danh Bun An Co <angco89@gmail.com>
2016-12-19 17:58:20 +0700

    share fb

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -639,12 +639,13 @@ class Api::V1::UserController < Api::V1::ApplicationController
 	def shareFBReceivedCoin
 		begin
 			graph = Koala::Facebook::API.new(params[:accessToken])
-			money = 20
 			info = graph.get_object(params[:post_id])
 			fb_id = params[:post_id].split("_")[0]
 			if params['room'].present? && params['room']['on_air'] == "true"
-				money = FbShareLog.where('user_id = ?', @user.id).count < 1 ? 100 : money
-				if FbShareLog.where('user_id = ? AND room_id = ? AND created_at > ?', @user.id, params['room']['id'].to_i, Time.now.beginning_of_day).count < 1
+				money = FbShareLog.where('user_id = ?', @user.id).count < 1 ? 100 : 20
+				if FbShareLog.where('fb_id = ? AND room_id = ? AND created_at > ?', fb_id, params['room']['id'].to_i, Time.now.beginning_of_day).count > 0
+					render plain: 'Facebook đã chia sẽ trước đó!!!', status: 200
+				elsif FbShareLog.where('user_id = ? AND room_id = ? AND created_at > ?', @user.id, params['room']['id'].to_i, Time.now.beginning_of_day).count < 1
 					@user.increaseMoney(money)
 					fb_logs(params[:post_id], money, fb_id, params['room']['id'].to_i)
 					render plain: 'Đã cộng tiền thành công!!!', status: 200
