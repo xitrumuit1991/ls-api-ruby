a5d1d25e83a1a222b51a292a28a2448f572c32bd
Danh Bun An Co <angco89@gmail.com>
2017-01-10 12:06:12 +0700

    bo ghi log

--- app/helpers/record_stream_helper.rb
+++ app/helpers/record_stream_helper.rb
@@ -2,22 +2,22 @@ module RecordStreamHelper
   def start_stream room
     $redis.set("stream_room_id:#{room.id}", {year: Time.now.year.to_s, month: Time.now.month.to_s, day: Time.now.day.to_s, hour: Time.now.hour.to_s, min: Time.now.min.to_s})
     linkRecode = "http://stream.livestar.vn:8086/livestreamrecord?app=livestar-open&streamname=#{room.id.to_s}&outputFile=#{room.id.to_s}_#{Time.now.year.to_s}_#{Time.now.month.to_s}_#{Time.now.day.to_s}_#{Time.now.hour.to_s+'_'+Time.now.min.to_s}.mp4&option=overwrite&action=startRecording"
-    stream_logger = Logger.new("#{Rails.root}/public/backups/StartStream.log")
-    stream_logger.info("ANGCO DEBUG StartLinkRecode: #{linkRecode} \n")
-    stream_logger.info("ANGCO DEBUG room: #{room} \n")
+    # stream_logger = Logger.new("#{Rails.root}/public/backups/StartStream.log")
+    # stream_logger.info("ANGCO DEBUG StartLinkRecode: #{linkRecode} \n")
+    # stream_logger.info("ANGCO DEBUG room: #{room} \n")
     stream_action linkRecode 
     return true
   end
 
   def end_stream room
     redis_stream = eval($redis.get("stream_room_id:#{room.id}"))
-    stream_logger = Logger.new("#{Rails.root}/public/backups/StopStream.log")
-    stream_logger.info("ANGCO DEBUG room: #{room} \n")
-    stream_logger.info("ANGCO DEBUG room: #{redis_stream} \n")
+    # stream_logger = Logger.new("#{Rails.root}/public/backups/StopStream.log")
+    # stream_logger.info("ANGCO DEBUG room: #{room} \n")
+    # stream_logger.info("ANGCO DEBUG room: #{redis_stream} \n")
     linkRecode = "http://stream.livestar.vn:8086/livestreamrecord?app=livestar-open&streamname=#{room.id.to_s}&outputFile=#{room.id.to_s}_#{redis_stream[:year]}_#{redis_stream[:month]}_#{redis_stream[:day]}_#{redis_stream[:hour]}_#{redis_stream[:min]}.mp4&option=overwrite&action=stopRecording"
     linkVideo = "http://stream.livestar.vn:80/livestar-vod/mp4:#{room.id.to_s}_#{redis_stream[:year]}_#{redis_stream[:month]}_#{redis_stream[:day]}_#{redis_stream[:hour]}_#{redis_stream[:min]}.mp4/playlist.m3u8"
-    stream_logger.info("ANGCO DEBUG StopLinkRecode: #{linkRecode} \n")
-    stream_logger.info("ANGCO DEBUG room: #{linkVideo} \n")
+    # stream_logger.info("ANGCO DEBUG StopLinkRecode: #{linkRecode} \n")
+    # stream_logger.info("ANGCO DEBUG room: #{linkVideo} \n")
     stream_action linkRecode 
     add_vod(linkVideo, room, redis_stream)
     return true
@@ -29,9 +29,9 @@ module RecordStreamHelper
     request = Net::HTTP::Post.new(uri.request_uri)
     request.basic_auth 'record', 'JmCpjEWHjcdO'
     response = http.request(request)
-    stream_logger = Logger.new("#{Rails.root}/public/backups/Response.log")
-    stream_logger.info("ANGCO DEBUG Response: #{response.to_yaml} \n")
-    stream_logger.info("ANGCO DEBUG Link: #{link} \n")
+    # stream_logger = Logger.new("#{Rails.root}/public/backups/Response.log")
+    # stream_logger.info("ANGCO DEBUG Response: #{response.to_yaml} \n")
+    # stream_logger.info("ANGCO DEBUG Link: #{link} \n")
   end
   
   def stream_action link
