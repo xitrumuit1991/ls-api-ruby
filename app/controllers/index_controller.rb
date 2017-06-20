6b6d5a558a7ef6a8566787e0e788cfcf83cd1688
nguyen@gmail.com <nguyen@gmail.com>
2017-06-20 14:50:04 +0700

    change message healthcheck LS AAPI

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -9,6 +9,6 @@ class IndexController < ApplicationController
 
   def healthcheck
     logger.info("---------api check healthcheck---------")
-    render plain: 'ok'
+    render json: { message: 'ok', service: "livestar api service", date: Time.now.strftime("%Y/%m/%d %H:%M") }, status: 200
   end
 end
