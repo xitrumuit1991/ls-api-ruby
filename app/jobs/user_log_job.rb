adde1793fba4584f7591b29040c380d96491be11
nguyentvk <tranvokhoinguyen@gmail.com>
2017-07-17 16:43:19 +0700

    live contrller fix

--- app/jobs/user_log_job.rb
+++ app/jobs/user_log_job.rb
@@ -1,13 +1,6 @@
 class UserLogJob < ActiveJob::Base
 	queue_as :default
 	def perform(user, room_id, total)
-		userlog = UserLog.new
-		userlog.create(user_id: user.id, room_id: room_id, money: total)
-		# Rails.logger.info("UserLogJob insert user_log")
-		# user.user_logs.create(room_id: room_id, money: total)
-		# Rails.logger.info("user.user_logs=#{user.user_logs}")
-		# if user.user_logs.present?
-		# 	Rails.logger.info("user.user_logs=#{user.user_logs.to_json}")
-		# end
+		user.user_logs.create(room_id: room_id, money: total)
 	end
 end
